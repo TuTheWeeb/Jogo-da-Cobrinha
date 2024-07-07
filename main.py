8ec9ff634d36855d5b1378df7378850d03ef4dc4
TuTheWeeb <eduardo.tuler@gmail.com>
2024-07-07 14:19:43 -0300

    Menu semi-finalizado

--- main.py
+++ main.py
@@ -14,50 +14,54 @@ class App():
         """
         # Definições
         self.master = master
+        self.master.config(height=GAME_HEIGHT, width=GAME_WIDTH)
         self.master.resizable(False, False)
         self.master.title("Jogo da Cobrinha")
         self.master.geometry(f"{GAME_WIDTH}x{GAME_HEIGHT}")
 
         # Cria as paginas ou Frames
-        self.Menu = Frame(self.master)
-        self.Jogo = Frame(self.master)
-        self.GameOver = Frame(self.master)
+        self.Menu = Frame(self.master, height=GAME_HEIGHT, width=GAME_WIDTH)
+        self.Jogo = Frame(self.master, height=GAME_HEIGHT, width=GAME_WIDTH)
+        self.GameOver = Frame(self.master, height=GAME_HEIGHT, width=GAME_WIDTH)
 
         # Coloca as paginas em modelo grid
-        #self.Menu.grid(row=0, column=0, sticky="nsew")
-        #self.Jogo.grid(row=0, column=0, sticky="nsew")
-        #self.GameOver.grid(row=0, column=0, sticky="nsew")
-        self.Menu.pack(fill="both", expand=True)
-        self.Jogo.pack(fill="both", expand=True)
-        self.GameOver.pack(fill="both", expand=True)
+        self.Menu.grid(row=0, column=0, sticky="nsew")
+        self.Jogo.grid(row=0, column=0, sticky="nsew")
+        self.GameOver.grid(row=0, column=0, sticky="nsew")
+        #self.Menu.pack(fill="both", expand=True, pady=100)
+        #self.Jogo.pack(fill="both", expand=True)
+        #self.GameOver.pack(fill="both", expand=True)
         self.menu()
 
     def menu(self):
         """Cria a janela do menu inicial"""
+
+        fonte=("Comic Sans", 15)
+
         self.Menu.tkraise()
-        self.nome = Label(self.Menu, text="Jogo da Cobrinha!")
-        self.nome.pack(pady=10)
+        self.nome = Label(self.Menu, text="Jogo da Cobrinha!", font=fonte)
+        self.nome.place(x=(GAME_WIDTH//2 - 90), y=10)
 
-        fonte_botao=("Comic Sans", 15)
-        self.botao_inciar = Button(self.Menu, text="Jogar", font=fonte_botao, height= 5, width=20, background= "#e361f4", command=self.jogar)
-        self.botao_inciar.pack(pady=GAME_HEIGHT//3, side="bottom")
+        y_botao = 0.6
 
-        self.botao_sair = Button(self.Menu, text="Sair", command=self.master.destroy)
-        self.botao_sair.pack(pady=GAME_HEIGHT//3, side="top")
+        self.botao_inciar = Button(self.Menu, text="Jogar", font=fonte, height=2, width=10, command=self.jogar)
+        self.botao_inciar.place(relx=0.3, rely=y_botao, anchor=CENTER)
+
+        self.botao_sair = Button(self.Menu, text="Sair", font=fonte, height=2, width=10, command=self.master.destroy)
+        self.botao_sair.place(relx=0.7, rely=y_botao, anchor=CENTER)
 
     def jogar(self): #nao ta funcionando como comando do botao
-        score = 0
         self.Jogo.tkraise()
-
+        self.score = 0
         #Label de pontuação
-        label = Label(self.Jogo, text="Score: {}".format(score),font=("consolas", 40))
-        label.pack()
+        self.label = Label(self.Jogo, text="Score: {}".format(self.score), font=("consolas", 40))
+        self.label.pack()
 
         #Criação da canvas 
-        canvas = Canvas(self.Jogo, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
-        canvas.pack()
+        self.canvas = Canvas(self.Jogo, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+        self.canvas.pack()
 
-        
+
 if __name__ == "__main__":
     master = Tk()
     app = App(master)
